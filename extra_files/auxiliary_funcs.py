72c01ba1f24cde02cd0ff190de96f7f75966ee15
Roberto Costa Bullitta <46348284+rocobull@users.noreply.github.com>
2023-09-20 17:00:00 +0100

    Add files via upload

--- extra_files/auxiliary_funcs.py
+++ extra_files/auxiliary_funcs.py
@@ -6,49 +6,55 @@ from sklearn.metrics import accuracy_score, recall_score, precision_score, matth
                             mean_squared_error, r2_score
 from scipy.stats import pearsonr, spearmanr
 
-from typing import Union
+from typing import Union, List, Tuple
 import itertools
 
 
 
 
-
-# def _check_repeats(param_dict):
-#     """
-#
-#     """
-#     if "multicol" in param_dict:
-#         if "multicol_correlation_method" in param_dict:
-#             if (param_dict["multicol"] is False) and (param_dict["multicol_correlation_method"].lower() != "pearson"):
-#                 return False
-#
-#     if "outliers" in param_dict:
-#         if "outlier_treat_method" in param_dict:
-#             if (param_dict["outliers"] is False) and (param_dict["outlier_treat_method"].lower() != "capping"):
-#                 return False
-#     #...
-#     return True
-
-def combs(params):
+def combs(params:dict) -> List[dict]:
     """
     Returns a list of dictionaries with all combinations of the given key-value pairs.
+
+    Parameters
+    ----------
+    params: dict
+        A dictionary containing hyperparameter names as keys, and a list of their possible values as values.
+
+    Returns
+    -------
+    combinations: List[dict]
+        A list of dictionaries, each with a different combination of hyperparameter values.
     """
     val_combinations = itertools.product(*params.values())
 
     output = []
     for val in val_combinations:
         new_dict = {}
-        for k,v in zip(params.keys(), val):
+        for k, v in zip(params.keys(), val):
             new_dict[k] = v
-        #if _check_repeats(new_dict):
         output.append(new_dict)
 
     return output
 
 
-def x_y_split(data:pd.DataFrame, y_name:Union[int,str]):
+def x_y_split(data:pd.DataFrame, y_name:Union[int,str]) -> Tuple[pd.DataFrame, pd.Series]:
     """
+    Separates the independent variables from the dependent variable.
 
+    Parameters
+    ----------
+    data: pd.DataFrame
+        The data.
+    y_name: Union[int,str]
+        The dependent variable's column name or index.
+
+    Returns
+    -------
+    X: pd.DataFrame
+        The independent variable data.
+    y: pd.Series
+        The dependent variable data.
     """
     if type(y_name) is int:
         y = pd.DataFrame(data.iloc[:, y_name])
@@ -61,8 +67,22 @@ def x_y_split(data:pd.DataFrame, y_name:Union[int,str]):
     return X, y
 
 
-def get_binary_scores(y:pd.Series, y_pred:np.ndarray):
+def get_binary_scores(y:pd.Series, y_pred:np.ndarray) -> Tuple[float, float, float, float, float]:
+    """
+    Returns the accuracy, recall, specificity, precision and MCC metrics for the 'y' binary value predictions.
 
+    Parameters
+    ----------
+    y: pd.Series
+        The true 'y' values.
+    y_pred
+        The predicted 'y' values.
+
+    Returns
+    -------
+    scores: Tuple[float, float, float, float, float]
+        The accuracy, recall, specificity, precision and MCC metric values
+    """
     accuracy = accuracy_score(y, y_pred)
     recall = recall_score(y, y_pred)
     specificity = recall_score(y, y_pred, pos_label=0) #Same as recall, but for negative samples
@@ -70,15 +90,15 @@ def get_binary_scores(y:pd.Series, y_pred:np.ndarray):
     mcc = matthews_corrcoef(y, y_pred)
 
     # predictions = [y.values[i] == y_pred[i] for i in range(len(y))]
-    #
+
     # positive_mask = [i for i, val in enumerate(y.values) if (int(val) == 1)]
     # negative_mask = [i for i, val in enumerate(y.values) if (int(val) == 0)]
-    #
+
     # TP = sum([1 if val else 0 for i, val in enumerate(predictions) if (i in positive_mask)]) #VP
     # TN = sum([1 if val else 0 for i, val in enumerate(predictions) if (i in negative_mask)]) #VN
     # FP = sum([1 if not val else 0 for i, val in enumerate(predictions) if (i in negative_mask)])
     # FN = sum([1 if not val else 0 for i, val in enumerate(predictions) if (i in positive_mask)])
-    #
+
     # accuracy = (TN+TP)/(TN+TP+FP+FN)
     # recall = TP/(FN+TP)
     # specificity = TN/(TN+FP)
@@ -89,8 +109,22 @@ def get_binary_scores(y:pd.Series, y_pred:np.ndarray):
 
 
 
-def get_regression_scores(y:pd.Series, y_pred:np.ndarray):
+def get_regression_scores(y:pd.Series, y_pred:np.ndarray) -> Tuple[float, float, float, float, float]:
+    """
+    Returns the RMSE, MSE, r2, Pearson correlation and Spearman correlation metrics for the 'y' real value predictions.
 
+    Parameters
+    ----------
+    y: pd.Series
+        The true 'y' values.
+    y_pred
+        The predicted 'y' values.
+
+    Returns
+    -------
+    scores: Tuple[float, float, float, float, float]
+        The RMSE, MSE, r2, Pearson correlation and Spearman correlation metric values
+    """
     rmse = mean_squared_error(y, y_pred, squared=False)
     mse = mean_squared_error(y, y_pred, squared=True)
     r2 = r2_score(y, y_pred)
@@ -134,5 +168,5 @@ PRESETS = ['all', 'performance', 'physico-chemical']
                 #'seq-order',
            #'modlamp-correlation', 'modlamp-all']
 
-for params in VARIABLE_PARAMS:
-   print(params)
+# for params in VARIABLE_PARAMS:
+#    print(params)
